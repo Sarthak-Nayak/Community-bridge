ef9a50c095cc08a1dc2945f9cd7ed2d7d7a2a2bc
Sarthak-Nayak <nsarthak458@gmail.com>
2026-04-01 09:21:22 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ const PlaceholderIndex = () => {
       <img data-lovable-blank-page-placeholder="REMOVE_THIS" src="/placeholder.svg" alt="Your app will live here!" />
     </div>
   );
-};
+}; 
 
 const Index = PlaceholderIndex;
 
